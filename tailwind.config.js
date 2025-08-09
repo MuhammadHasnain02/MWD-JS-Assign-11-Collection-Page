22f68d6b24c3df0dcc5d5dec0f5649cdf6f43eda
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-08-09 12:51:36 +0500

    C-P

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-content: ["./index.html", "./app.js"],
-theme: {
-extend: {},
-},
-plugins: [],
+    content: ["./index.html", "./app.js"],
+    theme: {
+    extend: {},
+    },
+    plugins: [],
 }   
